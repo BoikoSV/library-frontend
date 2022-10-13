f886dbe1595de1bd86614d95e7cf16f45c25e4ce
Sergii Boiko <boikosmail2@gmail.com>
2022-10-13 19:54:33 +0300

    Add empty record to tables

--- components/books/BooksTable.vue
+++ components/books/BooksTable.vue
@@ -1,49 +1,52 @@
 <template>
-  <table class="table align-middle table-bordered table-hover position-relative">
-    <div v-if="load" class="position-absolute top-0 bottom-0 start-0 end-0 d-flex justify-content-center align-items-center" style="min-height: 100px;background-color: rgba(0, 0, 0, 0.3)">
-      <div class="spinner-border text-light" style="height: 5rem; width: 5rem;" role="status">
-        <span class="visually-hidden">Loading...</span>
+  <div>
+    <div v-if="books.length === 0" class="fs-3 text-center py-4">There are no records!</div>
+    <table v-else class="table align-middle table-bordered table-hover position-relative">
+      <div v-if="load" class="position-absolute top-0 bottom-0 start-0 end-0 d-flex justify-content-center align-items-center" style="min-height: 100px;background-color: rgba(0, 0, 0, 0.3)">
+        <div class="spinner-border text-light" style="height: 5rem; width: 5rem;" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
       </div>
-    </div>
-    <thead>
-    <tr class="text-nowrap text-center text-muted">
-      <th>Cover</th>
-      <th>ISBN</th>
-      <th>
-        Title
-        <i class="bi bi-caret-down-fill" />
-      </th>
-      <th>
-        Author
-        <i class="bi bi-caret-down-fill" />
-      </th>
-      <th>
-        Status
-      </th>
-      <th>--</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr v-for="book in books" :key="book.id">
-      <td>Image</td>
-      <td>{{ book.isbn }}</td>
-      <td>{{ book.title }}</td>
-      <td>{{ book.author }}</td>
-      <td v-html="status(book.status)" class="text-center"></td>
-      <td class="text-nowrap d-flex justify-content-center">
-        <nuxt-link :to="`/books/${book.id}`" class="link-primary px-2"><i class="bi bi-eye-fill"></i></nuxt-link>
-        <nuxt-link :to="`/books/${book.id}/edit`" class="link-primary px-2"><i class="bi bi-pencil-fill"></i></nuxt-link>
-        <button
-          @click="$emit('delete', book.id)"
-          type="button"
-          class="link-danger bg-transparent border-0 px-2"
-        >
-          <i class="bi bi-trash-fill"></i>
-        </button>
-      </td>
-    </tr>
-    </tbody>
-  </table>
+      <thead>
+      <tr class="text-nowrap text-center text-muted">
+        <th>Cover</th>
+        <th>ISBN</th>
+        <th>
+          Title
+          <i class="bi bi-caret-down-fill" />
+        </th>
+        <th>
+          Author
+          <i class="bi bi-caret-down-fill" />
+        </th>
+        <th>
+          Status
+        </th>
+        <th>--</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="book in books" :key="book.id">
+        <td>Image</td>
+        <td>{{ book.isbn }}</td>
+        <td>{{ book.title }}</td>
+        <td>{{ book.author }}</td>
+        <td v-html="status(book.status)" class="text-center"></td>
+        <td class="text-nowrap d-flex justify-content-center">
+          <nuxt-link :to="`/books/${book.id}`" class="link-primary px-2"><i class="bi bi-eye-fill"></i></nuxt-link>
+          <nuxt-link :to="`/books/${book.id}/edit`" class="link-primary px-2"><i class="bi bi-pencil-fill"></i></nuxt-link>
+          <button
+            @click="$emit('delete', book.id)"
+            type="button"
+            class="link-danger bg-transparent border-0 px-2"
+          >
+            <i class="bi bi-trash-fill"></i>
+          </button>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
--- components/users/UsersTable.vue
+++ components/users/UsersTable.vue
@@ -1,56 +1,59 @@
 <template>
-  <table class="table align-middle table-bordered table-hover position-relative">
+  <div>
+    <div v-if="users.length === 0" class="fs-3 text-center py-4">There are no records!</div>
+    <table v-else class="table align-middle table-bordered table-hover position-relative">
     <div v-if="load" class="position-absolute top-0 bottom-0 start-0 end-0 d-flex justify-content-center align-items-center" style="min-height: 100px;background-color: rgba(0, 0, 0, 0.3)">
-      <div class="spinner-border text-light" style="height: 5rem; width: 5rem;" role="status">
-        <span class="visually-hidden">Loading...</span>
+        <div class="spinner-border text-light" style="height: 5rem; width: 5rem;" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
       </div>
-    </div>
-    <thead>
-    <tr class="text-nowrap text-center text-muted">
-      <th>Avatar</th>
-      <th>
-        Email
-        <i class="bi bi-caret-down-fill" />
-      </th>
-      <th>
-        Name
-        <i class="bi bi-caret-down-fill" />
-      </th>
-      <th>
-        Roles
-      </th>
-      <th>
-        Status
-      </th>
-      <th>
-        Created at
-        <i class="bi bi-caret-down-fill" />
-      </th>
-      <th>--</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr v-for="user in users" :key="user.id">
-      <td>avatar img</td>
-      <td>{{ user.email }}</td>
-      <td class="text-nowrap">{{ user.name }}</td>
-      <td>{{ role(user.role) }}</td>
-      <td><span class="badge" :class="status(user.status).class">{{ status(user.status).text }}</span></td>
-      <td class="text-nowrap">{{ $moment(user.created_at).format("DD.MM.YY") }}</td>
-      <td class="text-nowrap d-flex justify-content-center">
-        <nuxt-link :to="`/users/${user.id}`" class="link-primary px-2"><i class="bi bi-eye-fill"></i></nuxt-link>
-        <nuxt-link :to="`/users/${user.id}/edit`" class="link-primary px-2"><i class="bi bi-pencil-fill"></i></nuxt-link>
-        <button
-          @click="$emit('delete', user.id)"
-          type="button"
-          class="link-danger bg-transparent border-0 px-2"
-        >
-          <i class="bi bi-trash-fill"></i>
-        </button>
-      </td>
-    </tr>
-    </tbody>
-  </table>
+      <thead>
+      <tr class="text-nowrap text-center text-muted">
+        <th>Avatar</th>
+        <th>
+          Email
+          <i class="bi bi-caret-down-fill" />
+        </th>
+        <th>
+          Name
+          <i class="bi bi-caret-down-fill" />
+        </th>
+        <th>
+          Roles
+        </th>
+        <th>
+          Status
+        </th>
+        <th>
+          Created at
+          <i class="bi bi-caret-down-fill" />
+        </th>
+        <th>--</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="user in users" :key="user.id">
+        <td>avatar img</td>
+        <td>{{ user.email }}</td>
+        <td class="text-nowrap">{{ user.name }}</td>
+        <td>{{ role(user.role) }}</td>
+        <td><span class="badge" :class="status(user.status).class">{{ status(user.status).text }}</span></td>
+        <td class="text-nowrap">{{ $moment(user.created_at).format("DD.MM.YY") }}</td>
+        <td class="text-nowrap d-flex justify-content-center">
+          <nuxt-link :to="`/users/${user.id}`" class="link-primary px-2"><i class="bi bi-eye-fill"></i></nuxt-link>
+          <nuxt-link :to="`/users/${user.id}/edit`" class="link-primary px-2"><i class="bi bi-pencil-fill"></i></nuxt-link>
+          <button
+            @click="$emit('delete', user.id)"
+            type="button"
+            class="link-danger bg-transparent border-0 px-2"
+          >
+            <i class="bi bi-trash-fill"></i>
+          </button>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
